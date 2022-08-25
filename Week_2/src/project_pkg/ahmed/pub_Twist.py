1b680dbeebb4af11400b0af30d0cb587daa76234
Ahmed Ayman <104770329+ahmed-ayman362@users.noreply.github.com>
2022-08-25 23:24:11 +0200

    Add files via upload

---
+++ Week_2/src/project_pkg/ahmed/pub_Twist.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+import rospy
+from geometry_msgs.msg import Twist
+
+if __name__ == '__main__':
+    rospy.init_node("draw_circle")
+    rospy.loginfo("node has been started.")
+    
+    pub = rospy.Publisher("/turtle1/cmd_vel",Twist,queue_size=10)
+    
+    rate = rospy.Rate(10)
+    
+    while not rospy.is_shutdown():
+        #publish cmd vel
+        msg = Twist()
+        msg.linear.x = 5.0
+        
+        
+        msg.angular.z = 6.0
+        
+        pub.publish(msg)
+        rate.sleep()
